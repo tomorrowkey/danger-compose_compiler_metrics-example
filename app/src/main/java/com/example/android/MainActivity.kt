76fea8c20fb9ed7dea5c0286aebc24319ad54f7b
Tomoki Yamashita <tomorrowkey@gmail.com>
2024-02-10 17:02:56 +0900

    Implement unskippable composable

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.android.ui.theme.ExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +24,7 @@ class MainActivity : ComponentActivity() {
             ExampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    ContactRow(contact = Contact(name = "Taro", number = "090-1234-5678"))
                 }
             }
         }
@@ -27,20 +32,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(
-    name: String,
+fun ContactRow(
+    contact: Contact,
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
+    var selected by remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExampleTheme {
-        Greeting("Android")
+    Row(modifier) {
+        ContactDetails(contact)
+        ToggleButton(selected, onToggled = { selected = !selected })
     }
 }
+
+@Composable
+fun ToggleButton(
+    selected: Boolean,
+    onToggled: (Boolean) -> Unit,
+) {
+    Switch(checked = selected, onCheckedChange = onToggled)
+}
+
+@Composable
+fun ContactDetails(contact: Contact) {
+    Text(text = "Name ${contact.name}, Number: ${contact.number}")
+}
+
+data class Contact(var name: String, val number: String)
